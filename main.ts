6c37c2679c01dbcf9a683dae897cd80339392cad
hohoihung <w10546.ho@gmail.com>
2023-02-28 11:14:09 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,23 @@
 function right (speed: number) {
     wuKong.setAllMotor(0 - speed, speed / 2)
 }
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 10) {
+        forward(100)
+    } else if (receivedNumber == 11) {
+        reverse(100)
+    } else if (receivedNumber == 12) {
+        left(100)
+    } else if (receivedNumber == 13) {
+        right(100)
+    } else if (receivedNumber == 14) {
+        left(50)
+    } else if (receivedNumber == 15) {
+        right(50)
+    } else {
+        wuKong.stopAllMotor()
+    }
+})
 function left (speed: number) {
     wuKong.setAllMotor(speed / 2, 0 - speed)
 }
@@ -28,4 +45,5 @@ function forward (speed: number) {
     wuKong.setAllMotor(speed, speed)
 }
 wuKong.setLightMode(wuKong.LightMode.BREATH)
+radio.setGroup(8)
 basic.showIcon(IconNames.Happy)
