d3b063862a450a5005182f1f78f10200dc9ef033
hohoihung <w10546.ho@gmail.com>
2023-02-28 22:17:41 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 10) {
-        forward(40)
-    } else if (receivedNumber == 11) {
         reverse(40)
+    } else if (receivedNumber == 11) {
+        forward(40)
     } else if (receivedNumber == 12) {
-        wuKong.setAllMotor(0, 40)
+        wuKong.setAllMotor(10, 40)
     } else if (receivedNumber == 13) {
-        wuKong.setAllMotor(40, 0)
+        wuKong.setAllMotor(40, 10)
     } else if (receivedNumber == 14) {
         wuKong.stopAllMotor()
     } else if (receivedNumber == 15) {
@@ -14,9 +14,9 @@ radio.onReceivedNumber(function (receivedNumber) {
     }
 })
 input.onButtonPressed(Button.A, function () {
-    forward(40)
-    basic.pause(2000)
     reverse(40)
+    basic.pause(2000)
+    forward(40)
     basic.pause(2000)
     wuKong.stopAllMotor()
 })
@@ -24,7 +24,7 @@ function spinLeft (speed: number) {
     wuKong.setAllMotor(0, 0 - speed)
 }
 function reverse (speed: number) {
-    wuKong.setAllMotor(0 - speed, 0 - speed)
+    wuKong.setAllMotor(speed, speed)
 }
 input.onButtonPressed(Button.AB, function () {
     wuKong.stopAllMotor()
@@ -40,7 +40,7 @@ function spinRight (speed: number) {
     wuKong.setAllMotor(0 - speed, 0)
 }
 function forward (speed: number) {
-    wuKong.setAllMotor(speed, speed)
+    wuKong.setAllMotor(0 - speed, 0 - speed)
 }
 wuKong.setLightMode(wuKong.LightMode.BREATH)
 radio.setGroup(8)
@@ -59,13 +59,10 @@ basic.forever(function () {
 basic.forever(function () {
     if (0 == pins.digitalReadPin(DigitalPin.P13)) {
         spinLeft(50)
-        basic.pause(1000)
-        forward(50)
+        forward(40)
     }
     if (0 == pins.digitalReadPin(DigitalPin.P12)) {
         spinRight(50)
-        basic.pause(1000)
-        forward(50)
-        basic.pause(500)
+        forward(40)
     }
 })
